c224ac9adb1995b9e124de69c764c9b022c0d166
lindelof <phragmen@protonmail.com>
2019-12-11 20:34:03 +0800

    add new version

---
+++ precache-manifest.57afbc3319fb955ad498671bc61fb7bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2592042f6ff533d4e185",
+    "url": "./static/css/main.a2f80409.chunk.css"
+  },
+  {
+    "revision": "2592042f6ff533d4e185",
+    "url": "./static/js/main.a7529ff6.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "154553bf630ae73e2817",
+    "url": "./static/js/2.f89f0f02.chunk.js"
+  },
+  {
+    "revision": "b0a9c9f19fe2b1d9468af4f422700686",
+    "url": "./index.html"
+  }
+];
